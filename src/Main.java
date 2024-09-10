f76df8f50de39cedfea5cdc665fc8619205ffbb0
SahilBas2005 <sahilbas2005@gmail.com>
2024-09-09 20:06:17 -0400

    Fix main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // This change was made on github. Hopefully it pulls
-        args = new String[1];
         FizzBuzz.doFizzBuzz(5);
         FizzBuzz.main(args);
     }
